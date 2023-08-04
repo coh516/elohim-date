8c41194cfe751ac695fb91f60839dfca3ee526ba
Seth Tenenbaum <coh516@gmail.com>
2023-08-03 20:11:53 -0400

    add support for leap year and days before jan 15

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # elohim-date
+convert the date to Elohim date
+@Todo: unit test for leap year and first day of year prior to jan 15'th
--- elohim-date.js
+++ elohim-date.js
@@ -1,7 +1,14 @@
 const elohimDate = () => {
-    const newYearDay = `${(new Date).getFullYear()}-01-15`;
-    const sunMonth = (month) => { const ny = new Date(newYearDay); ny.setDate(ny.getDate() +  73 * Math.floor(month/2) + (month % 2)*36 ); return ny };
+    const fullYear = (new Date).getFullYear();
+    const isLeapYear = ((fullYear % 4 == 0) && (fullYear % 100 != 0)) || (fullYear % 400 == 0);
+    (new Date).getMonth() === 1 && (new Date).getDate() < 15  && fullYear--;
+    const newYearDay = `${fullYear}-01-15`;
+    const sunMonth = (month) => { 
+        const ny = new Date(newYearDay); 
+        ny.setDate(ny.getDate() +  73 * Math.floor(month/2) + (month % 2)*(isLeapYear && month === 5 ? 37 : 36));
+        return ny;
+    };
     const sunWeek = () => { const ny = new Date(newYearDay); const week = (new Date() - ny.setDate(ny.getDate()) )/8.64e8; return { week: Math.floor(week), day: Math.floor(week-Math.floor(week)) }};
-    const sunDate = () => { const ny = new Date(newYearDay); let monthDay = (((new Date() - ny.setDate(ny.getDate()) )/.864e8)/(73/2)); let month = Math.floor(monthDay); let day = sunMonth(5).getDate(); return {year: (new Date).getFullYear()-34020, month, day, sunWeek: sunWeek().week, dayOfWeek: sunWeek().day}};
+    const sunDate = () => { const ny = new Date(newYearDay); let monthDay = (((new Date() - ny.setDate(ny.getDate()) )/.864e8)/(73/2)); let month = Math.floor(monthDay); let day = sunMonth(5).getDate(); return {year: (new Date).getFullYear()+34020, month, day, sunWeek: sunWeek().week, dayOfWeek: sunWeek().day}};
     return {sunMonth, sunWeek, sunDate}
 }
