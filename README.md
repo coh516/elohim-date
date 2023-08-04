c963dee142aa5f5506135c5eac18f2ac2fa91534
Seth Tenenbaum <coh516@gmail.com>
2023-08-03 20:45:34 -0400

    add details about the issues of the leap week

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 convert the date to Elohim date
 
 ## Todo: 
-    - week needs to carry over correctly from year to year
-    - unit test for leap year and first day of year prior to jan 15'th
+    - week needs to carry over correctly from year to year and have calculate a leap week during the leap month of the leap year
+    - unit tests
--- elohim-date.js
+++ elohim-date.js
@@ -3,12 +3,26 @@ const elohimDate = () => {
     const isLeapYear = ((fullYear % 4 == 0) && (fullYear % 100 != 0)) || (fullYear % 400 == 0);
     (new Date).getMonth() === 1 && (new Date).getDate() < 15  && fullYear--;
     const newYearDay = `${fullYear}-01-15`;
+
     const sunMonth = (month) => { 
         const ny = new Date(newYearDay); 
         ny.setDate(ny.getDate() +  73 * Math.floor(month/2) + (month % 2)*(isLeapYear && month === 5 ? 37 : 36));
         return ny;
     };
-    const sunWeek = () => { const ny = new Date(newYearDay); const week = (new Date() - ny.setDate(ny.getDate()) )/8.64e8; return { week: Math.floor(week), day: Math.floor(week-Math.floor(week)) }};
-    const sunDate = () => { const ny = new Date(newYearDay); let monthDay = (((new Date() - ny.setDate(ny.getDate()) )/.864e8)/(73/2)); let month = Math.floor(monthDay); let day = sunMonth(5).getDate(); return {year: (new Date).getFullYear()+34020, month, day, sunWeek: sunWeek().week, dayOfWeek: sunWeek().day}};
+
+    // sun weeks have one 11 day week during leap year
+    const sunWeek = () => { 
+        const ny = new Date(newYearDay); 
+        const week = (new Date() - ny.setDate(ny.getDate()) )/8.64e8; 
+        return { week: Math.floor(week), day: Math.floor(week-Math.floor(week)) }
+    };
+
+    const sunDate = () => { 
+        const ny = new Date(newYearDay); 
+        let monthDay = (((new Date() - ny.setDate(ny.getDate()) )/.864e8)/(73/2)); 
+        let month = Math.floor(monthDay); let day = sunMonth(5).getDate(); 
+        return {year: (new Date).getFullYear()+34020, month, day, sunWeek: sunWeek().week, dayOfWeek: sunWeek().day}
+    };
+
     return {sunMonth, sunWeek, sunDate}
 }
